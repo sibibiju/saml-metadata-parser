27db07e7711916a83ed3c94291c43a1fbc51b405
ksibibiju <51444735+ksibibiju@users.noreply.github.com>
2021-07-21 22:14:51 +0530

    Updated Files

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const parser = require('./lib/parser');
 const util = require('util');
 
 const getSAMLMetadata = async() => {
-    const metadata = await parser.parseFromFile(__dirname + '/lib/metadata.xml');
-    console.log(util.inspect(metadata, false, null, true));
+    try {
+        const metadata = await parser.parseFromFile(__dirname + '/lib/metadata.xml');
+        console.log(util.inspect(metadata, false, null, true));
+    } catch (err) {
+        console.log('Error: ', err);
+    }
 }
 
 getSAMLMetadata();
