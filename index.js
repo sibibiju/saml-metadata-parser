7c7d67b0a5d2c3bbf66b9b27770dfb997f87d80e
ksibibiju <ksibibiju@gmail.com>
2021-07-22 09:48:50 +0530

    updated passport functionality

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const util = require('util');
 
 const getSAMLMetadata = async() => {
     try {
-        const metadata = await parser.parseFromFile(__dirname + '/lib/metadata.xml');
+        const metadata = await parser.parseFromFile(__dirname + '/lib/metadata.xml', __dirname + '/lib/sp.xml', true);
         console.log(util.inspect(metadata, false, null, true));
     } catch (err) {
         console.log('Error: ', err);
