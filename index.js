d67bdb5aa5a150c8ac322a09677f836dc11b734d
ksibibiju <ksibibiju@gmail.com>
2021-07-29 19:08:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const parser = require('./lib/parser');
-const util = require('util');
-
-const getSAMLMetadata = async() => {
-    try {
-        const metadata = await parser.parseFromFile(__dirname + '/lib/metadata.xml', __dirname + '/lib/sp.xml', true);
-        console.log(util.inspect(metadata, false, null, true));
-    } catch (err) {
-        console.log('Error: ', err);
-    }
-}
-
-getSAMLMetadata();
+module.exports = require('./lib/parser');
